7085257441fc249fd4671e9bc30c3cac0e444a3b
Amrut Sagar <amrutsagar06@gmail.com>
2022-08-30 16:59:50 +0530

    state

--- native-en/App.js
+++ native-en/App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, Linking, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Mash");
+  const [session, setSession] = useState({ number: 6, title: "state" });
+  const [current, setCurrent] = useState(true);
+
+  const onClickHandler = () => {
+    setName("Programming with Amrut");
+    setSession({ number: 7, title: "state changed" });
+    setCurrent(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>This is {name}'s channel</Text>
+      <Text style={styles.text}>
+        This is session number {session.number} {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? "current session" : "next session"}
+      </Text>
+      <Button
+        style={styles.btn}
+        title="click to check"
+        onPress={onClickHandler}
+      ></Button>
     </View>
   );
 }
+//#C4E8C2 #AECFA4 #6BBD99
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#C4E8C2",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#6BBD99",
+    margin: 10,
   },
 });
